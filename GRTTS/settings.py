2a11677720c1dc19a581e7b1daa644f6ab140741
bufleek <brunookiprop@gmail.com>
2022-10-21 15:45:17 +0300

    Added custom user model

--- GRTTS/settings.py
+++ GRTTS/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'accounts.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
