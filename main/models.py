7b30e60eeba0bd7b18a527e308c77a2ba57e50c5
bufleek <brunookiprop@gmail.com>
2022-10-28 14:56:30 +0300

    timelog model defined

--- main/models.py
+++ main/models.py
@@ -1,4 +1,10 @@
+from typing import TYPE_CHECKING, Optional
+
 from django.db import models
+
+#avoiding circular imports with accounts.Models
+if TYPE_CHECKING:
+    from accounts.models import User
 
 
 # Create your models here.
@@ -7,3 +13,9 @@ class Office(models.Model):
     latitude = models.DecimalField(decimal_places=15, max_digits=20, null=False, blank=False)
     longitude = models.DecimalField(decimal_places=15, max_digits=20, null=False, blank=False)
     geo_radius = models.DecimalField(decimal_places=12, max_digits=20, null=False, blank=False)
+    
+class TimeLog(models.Model):
+    user: Optional["User"] = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    time_in = models.DateTimeField(null=True, blank=True)
+    time_out = models.DateTimeField(null=True, blank=True)
