3c7c7050cda5cbd296e351fdf069e179b4a5f2ea
bufleek <brunookiprop@gmail.com>
2022-10-28 14:13:27 +0300

    added offices field to user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from main.models import Office
 
 
 class User(AbstractUser):
-    pass
+    offices = models.ManyToManyField(Office)
